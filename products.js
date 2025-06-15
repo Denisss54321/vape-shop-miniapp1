3ab24417b179783e9cc9187349ae74d38bc62ccd
Denisss54321 <Rekrutf11@gmail.com>
2025-06-15 16:56:54 +0500

    products.js

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ const products = [
     id: "waka10000",
     name: "WAKA 10000",
     price: 4900,
-    image: "waka10000.jpeg",
+    image: "waka10000.JPEG",
     description: "Электронная сигарета WAKA на 10000 затяжек. Долговечный и насыщенный вкус.",
     flavors: ["Манго", "Ледяная клубника", "Двойное яблоко", "Голубика"]
   }
